4ec59f5dd8c6fd07fb9f09a97989a39698ab0d4c
Gage Parsons <gwparsons2007@gmail.com>
2025-11-25 22:22:31 -0500

    add platform moving

--- team-bird-a4-jumper/Platform.cs
+++ team-bird-a4-jumper/Platform.cs
@@ -10,10 +10,22 @@ namespace team_bird_a4_jumper
 {
     internal class Platform
     {
+        private float speed = 5f;
+        private float minX = 100f;
+        private float maxX = 400f;
+        private int direction = 1;
+
         Vector2 block = new Vector2(100, 900);
 
         public void Update() 
         {
+            block.X += speed * direction;
+           
+            if (block.X >= maxX || block.X <= minX)
+            {
+                direction *= -1;
+            }
+
             Draw.Capsule(block.X, block.Y, block.X + 100, block.Y, 30);
         }
     }
